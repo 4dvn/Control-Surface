601b2ea39c5f90d925d069c86b11a60aa0e517c1
4dvn <vuha.hau@hyper.vn>
2020-02-14 18:42:38 +0700

    Update Custom.ino

--- examples/Custom/Custom.ino
+++ examples/Custom/Custom.ino
@@ -1,6 +1,18 @@
+#include <Encoder.h>
 #include <Control_Surface.h>
 
 USBMIDI_Interface midi;
+
+CCPotentiometer potentiometer = {
+  A0,                                   // Analog pin connected to potentiometer
+  {MIDI_CC::Channel_Volume, CHANNEL_1}, // Channel volume of channel 1
+};
+
+CCRotaryEncoder enc = {
+  {6, 7},       // pins
+  MCU::V_POT_1, // MIDI address (CC number + optional channel)
+  1,            // optional multiplier if the control isn't fast enough
+};
 
 // The note numbers corresponding to the buttons in the matrix
 const AddressMatrix<2, 2> addresses = {{
@@ -16,6 +28,7 @@ NoteButtonMatrix<2, 2> buttonmatrix = {
 };
 
 void setup() {
+  relativeccsender::setMode(relativeCCmode::MACKIE_CONTROL_RELATIVE);
   Control_Surface.begin();
 }
 
